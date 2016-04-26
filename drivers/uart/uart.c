21d3175431899ff8e10a6a55cf79953ed1f92117
jaumeolivepetrus <jaumeolivepetrus@192.168.1.42>
2016-04-26 09:53:03 +0200

    Now io.receive function clears the received buffer before init the reception of the file.

--- drivers/uart/uart.c
+++ drivers/uart/uart.c
@@ -487,7 +487,9 @@ u8_t uart_read(u8_t unit, char *c, uint32_t timeout) {
 
 // Consume all received bytes, and do not nothing with them
 void uart_consume(u8_t unit) {
-    xQueueReset(uart[unit].q);
+    char tmp;
+    
+    while(uart_read(unit,&tmp,1));
 } 
 
 // Reads a string from the UART, ended by the CR + LF character
