58f938ef589b8218571ca56302c06684b82cdd92
jolive <jolive@iberoxarxa.com>
2016-05-12 02:12:07 +0200

    Theorical implementation of i2c primitives in I2C driver (not tested)

--- drivers/i2c/i2c.c
+++ drivers/i2c/i2c.c
@@ -27,7 +27,112 @@
  * this software.
  */
 
+#include "whitecat.h"
+
+#include <unistd.h>
+#include <syslog.h>
+#include <drivers/error.h>
+#include <drivers/cpu/resource.h>
 #include <drivers/i2c/i2c.h>
+#include <machine/pic32mz.h>
 
+tdriver_error *i2c_setup(int unit, int speed) {
+    unit--;
+    
+    // TO DO
+    // Lock resources used
+    
+    // Configure module
+    *(&I2C1CON + unit * 0x200) = 0;
+    
+    *(&I2C1CON + unit * 0x200) = (1 << 15) | // Turn on module
+                                 (1 << 13) | // Stop on idle mode
+                                 (1 << 9);   // Slew rate control disabled
+            
+    // Set BRG
+    *(&I2C1BRG + unit * 0x200) = (((((double)1.0/(((double)2.0) * ((double)speed))) - ((double)0.00000014)) * ((double)PBCLK2_HZ)) - ((double)2.0));
+    
+    return NULL;
+}
 
+void i2c_start(int unit) {            
+    // Set start condition
+    *(&I2C1CONSET + unit * 0x200) = (1 << 0);
+    
+    // Waiting for done
+    while (*(&I2C1CON + unit * 0x200) & (1 << 0));
+}
 
+void i2c_stop(int unit) {
+    // Wait for master logic inactive
+    while (*(&I2C1CON + unit * 0x200) & (0b11111));    
+
+    // Set stop condition
+    *(&I2C1CONSET + unit * 0x200) = (1 << 2);
+
+    // Waiting for done
+    while (*(&I2C1CON + unit * 0x200) & (1 << 2));
+}
+
+void i2c_send_address_write(int unit, char address) {
+    // Send address to slave with a write indication
+    *(&I2C1TRN + unit * 0x200) = (address << 1);
+
+    // Waiting for done
+    while (*(&I2C1STAT + unit * 0x200) & (1 << 0));
+    
+    // Waiting for ACK
+    while (*(&I2C1STAT + unit * 0x200) & (1 << 15));
+}
+
+void i2c_send_address_read(int unit, char address) {
+    // Send address to slave with a read indication
+    *(&I2C1TRN + unit * 0x200) = (address << 1) & 0x01;
+
+    // Waiting for done
+    while (*(&I2C1STAT + unit * 0x200) & (1 << 0));
+    
+    // Waiting for ACK
+    while (*(&I2C1STAT + unit * 0x200) & (1 << 15));
+}
+
+void i2c_send_ack(int unit) {
+    // Wait for master logic inactive
+    while (*(&I2C1CON + unit * 0x200) & (0b11111));    
+
+    // Enable ACK
+    *(&I2C1CONSET + unit * 0x200) = (1 << 4);
+    
+    // Waiting for done
+    while (*(&I2C1CON + unit * 0x200) & (1 << 4));    
+}
+
+void i2c_send_nack(int unit) {
+    // Wait for master logic inactive
+    while (*(&I2C1CON + unit * 0x200) & (0b11111));    
+
+    // Enable NACK
+    *(&I2C1CONSET + unit * 0x200) = (1 << 5);
+    
+    // Waiting for done
+    while (*(&I2C1CON + unit * 0x200) & (1 << 5));    
+}
+
+void i2c_read(int unit, char *data) {
+    unit--;
+    
+    // Enable receive
+    *(&I2C1CONSET + unit * 0x200) = (1 << 3);
+
+    // Waiting for done
+    while (*(&I2C1CON + unit * 0x200) & (1 << 3));    
+
+    // Waiting for data
+    while (!(*(&I2C1STAT + unit * 0x200) & (1 << 1)));    
+   
+    *data = *(&I2C1RCV + unit * 0x200);
+}
+
+void i2c_write(int unit) {
+    unit--;
+}
--- main/build.h
+++ main/build.h
@@ -1,5 +1,5 @@
 #ifndef BUILD_H
 #define BUILD_H
 #define BUILD_TIME \
-1462536089
+1463011875
 #endif
